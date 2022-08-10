f772dc09403f8e91a199fc79121630f4c77c99e5
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-10 00:45:57 -0300

    comentarios, textos e numeros

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 fun main() {
+    // val é uma variavel imutavel
+    // var é uma variavel mutavel
+
+    val prince = 31.90
     val product = "iMac"
+    val age = 31
+
+    println(prince)
     println(product)
+    println(age)
 }
