c44e2e3ddcf0ce7689f6d219b41fbb35f38efe41
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-10 01:00:20 -0300

    tipos primitivos

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -2,11 +2,18 @@ fun main() {
     // val é uma variavel imutavel
     // var é uma variavel mutavel
 
-    val prince = 31.90
-    val product = "iMac"
-    val age = 31
+    val preco = 31.90  // TIPO INFERIDO
 
-    println(prince)
+    // TIPOS PRIMITIVOS
+    // ESPAÇOS DE MEMORIA EM TIPOS PRIMITIVOS
+
+    val price: Double = 31.90
+    val product: String = "iMac"
+    val age: Int = 31
+    val byte : Byte = 8
+
+    println(preco)
+    println(price)
     println(product)
     println(age)
 }
