10e703fc55ecafe83c91a0d7fc6372d349c3b78b
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-24 18:31:03 -0300

    acessando propriedades

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -6,13 +6,15 @@ fun main() {
     //criptografia
     // char
 
-    val name = "Sinng" // string (sequencia de caracteres (chars))
-    val sobrenome = "Silva"
-    val primeiraLetra = '\t' // 16-bit
-    println(name + primeiraLetra + sobrenome)
 
-    // \n pula uma linha
-    // \t tabelacao
-    // '' - char
-    // "" - string
+    val name = "Sinng Chuk" // Objeto=tipo
+    val tamanhoDoNome = name.length // acessar a prorpiedade de um objeto (String)
+    println(tamanhoDoNome)
+    println("Ola Pessoal".length)
+
+    val novoNome = name.uppercase() // funcao que muda o comportamento de um objeto (string)
+    println(novoNome)
+    val novoNome2 = name.lowercase()
+    println(novoNome2)
+
 }
