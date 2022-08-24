fb2bf07fb2ad82de823f4e79d820437ca6b70456
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-24 18:15:46 -0300

    operadores logicos

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -2,7 +2,36 @@ fun main() {
     //GLOSSARIO (var, val, fun, class, double, String, int, false, true)
 
     // NUMEROS GRANDES
-    val numero : Long = 1_123_321_123
+    val numero: Long = 1_123_321_123
     println(numero)
+
+    // OPERADORES LOGICOS
+    var idade = 31
+
+    // SOMA
+    idade = idade + 10
+    println(idade)
+
+    // SUBTRACAO
+    idade = idade - 10
+    println(idade)
+
+    // DIVISAO
+    idade = idade / 10
+    println(idade)
+
+    //MULTIPLICACAO
+    idade = idade * 10
+    println(idade)
+
+    //TEXTOS
+    var textoIdade = "10"
+    println(textoIdade + " minha idade")
+
+    val booleana = true
+    // true / false ( 1 - 0 )
+    println(booleana)
+    println(!booleana)
+
 
 }
