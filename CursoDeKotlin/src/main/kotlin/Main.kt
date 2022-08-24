3935974d487c1b25b001ee5a2160272c34534438
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-24 18:07:20 -0300

    numeros vs textos

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -1,34 +1,15 @@
 fun main() {
-    // val é uma variavel imutavel
-    // var é uma variavel mutavel
+ //GLOSSARIO (var, val, fun, class, double, String, int, false, true)
 
-    val preco = 31.90  // TIPO INFERIDO
+    val numero = "10" + "10"
+    println(numero)
 
-    // TIPOS PRIMITIVOS
-    // ESPAÇOS DE MEMORIA EM TIPOS PRIMITIVOS
+    val numero2 = 1 + 1
+    println(numero2)
 
-    val byte: Byte = 8 // 8 bit
-    val short: Short = 16 // 16 bit
-    val int: Int = 32 // 32 bit (usado 99% do tempo)
-    val long : Long = 123185648949789798 // 64 bit
-
-    // BOOLEANO (true | false)
-    val booleano = true // 1 byte
-
-    // PADRAO CAMEL CASE (voceDeveEscreverDessaManeira)
-    val mediaDePreco = 22
-
-    // . apos uma variavel instrucoes e comandos
-    val resposta = mediaDePreco.toDouble()
-    println(resposta)
-
-    val price: Double = 31.90 // 64 bit ponto flutuante (floating point)
-    val product: String = "iMac"
-    val age: Int = 31
+    val resposta = numero2.toString()
+    println(numero2)
 
 
-    println(preco)
-    println(price)
-    println(product)
-    println(age)
+
 }
