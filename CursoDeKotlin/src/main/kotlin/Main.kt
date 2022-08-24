6eac47410cdd04ff022e00175d75f461ec04f7e6
Ingrid Silva <32372431+sinngjpeg@users.noreply.github.com>
2022-08-24 18:24:41 -0300

    caracteres especiais e charsa

--- CursoDeKotlin/src/main/kotlin/Main.kt
+++ CursoDeKotlin/src/main/kotlin/Main.kt
@@ -1,37 +1,18 @@
 fun main() {
     //GLOSSARIO (var, val, fun, class, double, String, int, false, true)
 
-    // NUMEROS GRANDES
-    val numero: Long = 1_123_321_123
-    println(numero)
+    // int, double, string, byte, long, float, boolean
 
-    // OPERADORES LOGICOS
-    var idade = 31
+    //criptografia
+    // char
 
-    // SOMA
-    idade = idade + 10
-    println(idade)
+    val name = "Sinng" // string (sequencia de caracteres (chars))
+    val sobrenome = "Silva"
+    val primeiraLetra = '\t' // 16-bit
+    println(name + primeiraLetra + sobrenome)
 
-    // SUBTRACAO
-    idade = idade - 10
-    println(idade)
-
-    // DIVISAO
-    idade = idade / 10
-    println(idade)
-
-    //MULTIPLICACAO
-    idade = idade * 10
-    println(idade)
-
-    //TEXTOS
-    var textoIdade = "10"
-    println(textoIdade + " minha idade")
-
-    val booleana = true
-    // true / false ( 1 - 0 )
-    println(booleana)
-    println(!booleana)
-
-
+    // \n pula uma linha
+    // \t tabelacao
+    // '' - char
+    // "" - string
 }
